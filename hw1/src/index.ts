113612efe87176ec482a8eb51b3b07c06632171f
Alex Martynov <a.martynov@216base.ru>
2022-03-03 16:27:49 +0300

    add setup's to configFile's

--- hw1/src/index.ts
+++ hw1/src/index.ts
@@ -1,14 +1,19 @@
+/* eslint-disable indent */
+/* eslint-disable no-undef */
 import { renderSearchFormBlock } from './search-form.js'
 import { renderSearchStubBlock } from './search-results.js'
 import { renderUserBlock } from './user.js'
 import { renderToast } from './lib.js'
 
-window.addEventListener('DOMContentLoaded', () => {
-  renderUserBlock('0')
-  renderSearchFormBlock()
-  renderSearchStubBlock()
-  renderToast(
+// eslint-disable-next-line no-constant-condition
+if(typeof window) {
+  window.addEventListener('DOMContentLoaded', () => {
+    renderUserBlock('0')
+    renderSearchFormBlock()
+    renderSearchStubBlock()
+    renderToast(
       {text: 'Это пример уведомления. Используйте его при необходимости', type: 'success'},
       {name: 'Понял', handler: () => {console.log('Уведомление закрыто')}}
-  )
-})
+    )
+  })
+}
