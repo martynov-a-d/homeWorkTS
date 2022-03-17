076f3f24402f88b1ed36794f506aa084f537c098
Alex Martynov <a.martynov@216base.ru>
2022-03-17 19:25:36 +0300

    fn output log

--- hw1/src/search-form.ts
+++ hw1/src/search-form.ts
@@ -6,7 +6,7 @@ export function renderSearchFormBlock () {
     checkInDate: string,
     checkOutDate: string
   }
-  const test: SearchFormData = {
+  const initValue: SearchFormData = {
     city: 'Санкт-Петербург',
     checkInDate: '2021-05-11',
     checkOutDate: '2021-05-13'
@@ -19,7 +19,7 @@ export function renderSearchFormBlock () {
         <div class="row">
           <div>
             <label for="city">Город</label>
-            <input id="city" type="text" disabled value=${test.city} />
+            <input id="city" type="text" disabled value=${initValue.city} />
             <input type="hidden" disabled value="59.9386,30.3141" />
           </div>
           <!--<div class="providers">
@@ -30,11 +30,11 @@ export function renderSearchFormBlock () {
         <div class="row">
           <div>
             <label for="check-in-date">Дата заезда</label>
-            <input id="check-in-date" type="date" value=${test.checkInDate} min=${test.checkInDate} max="2021-06-30" name="checkin" />
+            <input id="check-in-date" type="date" value=${initValue.checkInDate} min=${initValue.checkInDate} max="2021-06-30" name="checkin" />
           </div>
           <div>
             <label for="check-out-date">Дата выезда</label>
-            <input id="check-out-date" type="date" value=${test.checkOutDate} min=${test.checkInDate} max="2021-06-30" name="checkout" />
+            <input id="check-out-date" type="date" value=${initValue.checkOutDate} min=${initValue.checkInDate} max="2021-06-30" name="checkout" />
           </div>
           <div>
             <label for="max-price">Макс. цена суток</label>
@@ -50,7 +50,13 @@ export function renderSearchFormBlock () {
   )
   const btnSearch = document.getElementById('btnSearch')
   btnSearch.addEventListener('click', (event) => {
-    console.log(test);
     event.preventDefault()
+    const checkInValue = <HTMLInputElement>document.getElementById('check-in-date')
+    const checkOutValue = <HTMLInputElement>document.getElementById('check-out-date')
+    const cityValue = <HTMLInputElement>document.getElementById('city')
+    initValue.checkInDate = `${checkInValue.value}`
+    initValue.checkOutDate = `${checkOutValue.value}`
+    initValue.city = `${cityValue.value}`
+    console.log(initValue);
   })
 }
